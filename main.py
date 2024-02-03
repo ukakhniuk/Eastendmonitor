4eb63bade23cc5e86924331a5597a33445a840b4
ukakhniuk <vladislavkohnuk2001@gmail.com>
2024-02-03 11:56:13 +0100

    Interface added

--- main.py
+++ main.py
@@ -4,12 +4,48 @@ from bs4 import BeautifulSoup
 import time
 import aiohttp
 from disnake.ext import commands
-TOKEN = str(input('Input your TOKEN: '))
-SERVER_ID = int(input('Input your server ID: '))
-CHANNEL_ID = int(input('Input your channel ID: '))
+from tkinter import *
+# from disnake.errors import LoginFailure - for new futures
+TOKEN = ''
+SERVER_ID = ''
+CHANNEL_ID = ''
+def login():
+    global TOKEN
+    global SERVER_ID
+    global CHANNEL_ID
+    TOKEN = entry_TOKEN.get()
+    SERVER_ID = int(entry_SERVER_ID.get())
+    CHANNEL_ID = int(entry_CHANNEL_ID.get())
+    root.destroy()
+    return TOKEN, SERVER_ID, CHANNEL_ID
+
+
+root = Tk()
+root.title('Eastendmonitor by V-Solutions')
+root.geometry('600x400')
+label_TOKEN = Label(root, text="TOKEN:")
+label_TOKEN.grid(row=0, column=0, padx=10, pady=5, sticky="e")
+entry_TOKEN = Entry(root, width=70)
+entry_TOKEN.grid(row=0, column=1, padx=5, pady=5)
+label_SERVER_ID = Label(root, text="SERVER ID:")
+label_SERVER_ID.grid(row=1, column=0, padx=10, pady=5, sticky="e")
+entry_SERVER_ID = Entry(root, width=70)
+entry_SERVER_ID.grid(row=1, column=1, padx=10, pady=5)
+label_CHANNEL_ID = Label(root, text="CHANNEL ID:")
+label_CHANNEL_ID.grid(row=2, column=0, padx=10, pady=5, sticky="e")
+entry_CHANNEL_ID = Entry(root, width=70)
+entry_CHANNEL_ID.grid(row=2, column=1, padx=10, pady=5)
+continue_button = Button(root, text="Continue", command=login)
+continue_button.grid(row=3, columnspan=2, padx=10, pady=10)
+additional_text = "To start using the bot, you need to enter your bot's token here, your Discord server's ID,\n and also the ID of the channel where messages will be sent.\n\nTo get your bot's token, you need to go to discord.com, select the Developers section, create a bot there, \nadd it to your Discord server, and then copy the TOKEN from there.\n\nTo get your server's ID, you need to enable developer mode in Discord, right-click on your server, \nand copy the SERVER ID from there.\n\nTo get the CHANNEL ID for the bot, you need to right-click on the channel and copy its ID.\n\nOnce you have obtained this information, enter it in the fields above and click CONTINUE.\n\n Please note that if you make a mistake when entering this data, the application will not function correctly."
+additional_label = Label(root, text=additional_text, fg="black")
+additional_label.grid(row=4, columnspan=2, pady=10)
+root.mainloop()
 urls = []
 intents = disnake.Intents.all()
 previous_outputs = {}
+
+
 async def fetch_data(url, headers):
     while True:
         try:
@@ -26,6 +62,8 @@ async def fetch_data(url, headers):
             break
         except aiohttp.ClientConnectorError as e:
             print(f"Error: {e}")
+
+
 async def my_loop():
     global previous_output
     while True:
@@ -40,9 +78,11 @@ async def my_loop():
                 name = name.upper()
                 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"}
                 response = await fetch_data(url, headers)
+                await asyncio.sleep(1)
                 if url not in urls:
                     continue
-                ###PARSING START
+
+                #PARSING START
                 soup = BeautifulSoup(response, 'html.parser')
                 price = soup.find('span', class_='price').text
                 img1 = soup.find('div', class_='product media')
@@ -64,7 +104,8 @@ async def my_loop():
                         else:
                             size = size.replace(' - powiadom o dostępności', ' - 0')
                             output = output + size + '\n'
-                ###PARSING FINISH
+                #PARSING FINISH
+
                 if url not in previous_outputs or output != previous_outputs[url]:
                     previous_outputs[url] = output
                     channel = bot.get_channel(CHANNEL_ID)
@@ -78,8 +119,9 @@ async def my_loop():
             end_time = time.time()
             elapsed_time = end_time - start_time
             print(f"time: {elapsed_time:.2f} sec for {len(urls)} links.")
-            await asyncio.sleep(0)
-bot = commands.Bot(command_prefix=None, intents=intents, test_guilds=[SERVER_ID])
+bot = commands.Bot(command_prefix='/', intents=intents, test_guilds=[int(SERVER_ID)])
+
+
 @bot.event
 async def on_ready():
     await bot.loop.create_task(my_loop())
@@ -87,25 +129,33 @@ async def on_ready():
 async def on_message(message):
     if message.channel.id == CHANNEL_ID:
         await bot.process_commands(message)
+
+
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
         await ctx.send("Wrong command.")
+
+
 @bot.slash_command(name='url', description='Add URL for monitoring')
 async def set_url(ctx, new_url):
     global urls
     urls.append(new_url)
-    await ctx.send(f'new URL: {new_url}')
+    await ctx.send(f'new URL: <{new_url}>')
+
+
 @bot.slash_command(name='urls', description='Show all URLs that are currently being monitored')
 async def urls_list(ctx):
     global urls
     output = ''
     if len(urls) >= 1:
         for url in urls:
-            output = output + '\n' + url
+            output = output + '\n' + f'<{url}>'
         await ctx.send(f'URLs:{output}')
     else:
         await ctx.send('No monitored URLs.')
+
+
 @bot.slash_command(name='urls_clear', description='Remove every URL')
 async def urls_clear(ctx):
     global urls
@@ -114,4 +164,7 @@ async def urls_clear(ctx):
     await ctx.send('No monitored URLs anymore.')
     await asyncio.sleep(8)
     previous_outputs = {}
+
+
 bot.run(TOKEN)
+# отдельная функция с проверкой соединения, которая при провале останавливает активные асинхронные функции, но снова потом хуярит bot.run
